5bcaa566c9e8097f604a30ca76bc86068d0fafd1
Nanded <niksuf1@yandex.ru>
2023-04-26 01:01:38 +0300

    bug fix

--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,5 @@
-# Файл с клавиатурами
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-# Основная клавиатура
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 weather_button = KeyboardButton('Погода')
 exchange_button = KeyboardButton('Курсы валют')
@@ -9,7 +7,6 @@ random_img_button = KeyboardButton('Случайная картинка')
 help_button = KeyboardButton('Помощь')
 kb.add(weather_button).insert(exchange_button).insert(random_img_button).add(help_button)
 
-# Клавиатура для возвращения в основное меню
 kb_back = ReplyKeyboardMarkup(resize_keyboard=True)
 back_button = KeyboardButton('Назад')
 kb_back.add(back_button)
--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-# Главный файл
 from aiogram import Bot, Dispatcher, executor
 from config import telegram_token, proxy_url
 
-# Создание объекта бота
 bot = Bot(token=telegram_token, proxy=proxy_url)
 dp = Dispatcher(bot)
 
 
-# На старте в консоль писать сообщение
 async def on_startup(_):
     print('Бот запущен!')
 
 
-# Запуск бота
 if __name__ == '__main__':
     from handlers import dp
     executor.start_polling(dp, on_startup=on_startup)
